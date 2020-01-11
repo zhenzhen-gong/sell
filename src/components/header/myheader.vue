fe2fce040fa6432742ff2b8809814302fe44b22c
unknown <279836636@qq.com>
2020-01-11 17:21:52 +0800

    header allready

---
+++ src/components/header/myheader.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img width="64" height="64" :src="seller.avatar">
+      </div>
+    </div>
+    <div class="bulletin-wrapper"></div>
+</div>
+</template>
+<script>
+export default {
+  name: 'myheader',
+  props: {
+    seller: {
+    }
+  }
+}
+</script>
+<style lang='stylus' rel='stylesheet/stylus'>
+
+</style>
